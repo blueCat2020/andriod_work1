612e4c5231924ffa3e6bd477b7bae1cd61e9b613
blueCat2020 <3028152459@qq.com>
2020-09-16 10:15:14 +0800

    进行摄氏与华氏温度的转换

--- app/src/main/java/com/swufe/zbp/andriod_work1/MainActivity.java
+++ app/src/main/java/com/swufe/zbp/andriod_work1/MainActivity.java
@@ -2,6 +2,10 @@ package com.swufe.zbp.andriod_work1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btn1(View view) {
+        Log.i("main","btn1 msg ...");
+        EditText input=findViewById(R.id.inp);
+        TextView oup=findViewById(R.id.oup);
+        float temp=Float.valueOf(input.getText().toString());
+        float tran_temp=(float) (32+temp*1.8);
+        oup.setText(tran_temp+"华氏度");
     }
 }
