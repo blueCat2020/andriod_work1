170f42330be6a7e59fca96a1f6fb69804a20923a
blueCat2020 <3028152459@qq.com>
2020-09-21 21:21:56 +0800

    添加页面activity_work2,可进行两队同时累计计分

--- app/src/main/java/com/swufe/zbp/andriod_work1/MainActivity.java
+++ app/src/main/java/com/swufe/zbp/andriod_work1/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         Log.i("main","nextpage_btn msg ...");
 
 
-        setContentView(R.layout.activity_work1);
+        setContentView(R.layout.activity_work2);
     }
     public void work1_btn1(View view) {
         Log.i("work1","work1_btn1 msg ...");
@@ -61,5 +61,67 @@ public class MainActivity extends AppCompatActivity {
         TextView result_view=findViewById(R.id.work1_result);
         result_view.setText("0");
 
+    }
+
+    public void A_btn1(View view) {
+        Log.i("work2","work2_A_btn1 msg ...");
+        TextView result_view=findViewById(R.id.A_label2);
+        Button btn1=findViewById(R.id.A_btn1);
+        int result=Integer.valueOf(result_view.getText().toString());
+        result=result+3;
+        result_view.setText(""+result);
+    }
+
+    public void A_btn2(View view) {
+        Log.i("work2","work2_A_btn2 msg ...");
+        TextView result_view=findViewById(R.id.A_label2);
+        Button btn2=findViewById(R.id.A_btn2);
+        int result=Integer.valueOf(result_view.getText().toString());
+        result=result+2;
+        result_view.setText(""+result);
+    }
+
+    public void A_btn3(View view) {
+        Log.i("work2","work2_A_btn3 msg ...");
+        TextView result_view=findViewById(R.id.A_label2);
+        Button btn3=findViewById(R.id.A_btn3);
+        int result=Integer.valueOf(result_view.getText().toString());
+        result=result+1;
+        result_view.setText(""+result);
+    }
+
+    public void B_btn1(View view) {
+        Log.i("work2","work2_B_btn1 msg ...");
+        TextView result_view=findViewById(R.id.B_label2);
+        Button btn1=findViewById(R.id.B_btn1);
+        int result=Integer.valueOf(result_view.getText().toString());
+        result=result+3;
+        result_view.setText(""+result);
+    }
+
+    public void B_btn2(View view) {
+        Log.i("work2","work2_B_btn2 msg ...");
+        TextView result_view=findViewById(R.id.B_label2);
+        Button btn2=findViewById(R.id.B_btn2);
+        int result=Integer.valueOf(result_view.getText().toString());
+        result=result+2;
+        result_view.setText(""+result);
+    }
+
+    public void B_btn3(View view) {
+        Log.i("work2","work2_B_btn3 msg ...");
+        TextView result_view=findViewById(R.id.B_label2);
+        Button btn3=findViewById(R.id.B_btn3);
+        int result=Integer.valueOf(result_view.getText().toString());
+        result=result+1;
+        result_view.setText(""+result);
+    }
+
+    public void work2_reset_btn(View view) {
+        Log.i("work2","work2_reset_btn msg ...");
+        TextView result_view_A=findViewById(R.id.A_label2);
+        TextView result_view_B=findViewById(R.id.B_label2);
+        result_view_A.setText("0");
+        result_view_B.setText("0");
     }
 }
